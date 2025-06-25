6d50b84c58f4f9acc59819f20cc1100ef1821f6c
Ayoub <akhribi627@gmail.com>
2025-06-25 16:33:20 +0100

    add the quiz.js

--- quiz.js
+++ quiz.js
@@ -1,17 +1,15 @@
 function checkAnswer() {
   const correctAnswer = "4";
   const userAnswer = document.querySelector("quiz").value;
+}
 
-  if(userAnswer.checked === correctAnswer.checked){
+if (userAnswer.checked === correctAnswer){
     feedback.textContent = "Correct! Well done.";
     return;
   }else{
     feedback.textContent = "That's incorrect. Try again!";
     return;
   }
-
-}
-
 
 
 
